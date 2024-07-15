e8859c68e9a207f2ecef73f5b8b47b09c04ef0fc
root <git@minhvuc.com>
2024-07-15 09:01:48 +0000

    [FIX][REM] Fix web .dz, .cy whois. Remove .cv web whois.

--- webwhois/dz.py
+++ webwhois/dz.py
@@ -36,37 +36,34 @@ def whois_via_web(USER_AGENT, domain, domain_type):
     req = requests.Session()
     req_get = False
     try:
-        req_get = req.get('http://www.nic.dz/index.php?domain_name={0}&Submit=Rechercher&option=com_whois'.format(domain), headers=headers, verify=False)
+        req_get = req.get('https://api.nic.dz/v1/domains/{0}'.format(domain), headers=headers, verify=False)
     except:
         pass
     
     result = []
-    if req_get and req_get.status_code == 200 and req_get.text:
-        raw_data = req_get.text
+    if req_get and req_get.status_code >= 200 and req_get.status_code < 400 and req_get.json():
+        raw_data = req_get.json()
         if raw_data:
-            raw_data = raw_data.replace('\r', '')
-            raw_data = raw_data.replace('\n', '')
-            arr_multi_data = re.findall('<table cellspacing="0" cellpadding="0" width="55%">(.*?)</tbody></table>', raw_data, re.DOTALL|re.M)
-            for arr in arr_multi_data:
-                registrant_address = parse_dz_data('Description :(.*?)Contact Administratif :', arr)
-                admin_address = parse_dz_data('Contact Administratif :(.*?)Contact Technique :', arr)
-                tech_address = parse_dz_data('Contact Technique :(.*?)Registrar :', arr)
-                registrar = parse_dz_data('Registrar :(.*?)</td>', arr)
-                creation_date = parse_dz_data('<strong>Date de cr&eacute;ation  : </strong>(.*?)</td>', arr)
-                
-                if registrant_address:
-                    result.append('Registrant Address:\n\t{0}'.format(registrant_address))
-                if admin_address:
-                    result.append('Admin Address:\n\t{0}'.format(admin_address))
-                if tech_address:
-                    result.append('Tech Address:\n\t{0}'.format(tech_address))
-                if registrar:
-                    result.append('Registrar: {0}'.format(registrar))
-                if creation_date:
-                    result.append('Creation Date: {0}'.format(creation_date))
+            result.append('Registrant Organization: {0}'.format(raw_data.get('orgName', '')))
+            result.append('Registrant Address: {0}'.format(raw_data.get('addressOrg', '')))
+
+            result.append('Admin Organization: {0}'.format(raw_data.get('orgNameAdm', '')))
+            result.append('Admin Address: {0}'.format(raw_data.get('addressAdm', '')))
+            result.append('Admin Email: {0}'.format(raw_data.get('emailAdm', '')))
+            result.append('Admin Phone: {0}'.format(raw_data.get('phoneAdm', '')))
+            result.append('Admin Fax: {0}'.format(raw_data.get('faxAdm', '')))
+
+            result.append('Tech Organization: {0}'.format(raw_data.get('orgNameTech', '')))
+            result.append('Tech Address: {0}'.format(raw_data.get('addressTech', '')))
+            result.append('Tech Email: {0}'.format(raw_data.get('emailTech', '')))
+            result.append('Tech Phone: {0}'.format(raw_data.get('phoneTech', '')))
+            result.append('Tech Fax: {0}'.format(raw_data.get('faxTech', '')))
             
+            result.append('Registrar: {0}'.format(raw_data.get('registrar', '')))
+            result.append('Creation Date: {0}'.format(raw_data.get('creationDate', '')))
+    
     if result:
-        result.append('Full WHOIS: http://www.nic.dz')
+        result.append('Full WHOIS: https://www.nic.dz')
         final_result = {
             'status': True,
             'result': '\n'.join(result)
