9b5cbc4ecc5094b86bcd9ff896d1e8080555415c
Mai Tien Dung <git@minhvuc.com>
2025-07-13 08:36:37 +0700

    [REM] Remove unittest reserved a cv domain

--- tests/test_cctld_c.py
+++ tests/test_cctld_c.py
@@ -336,33 +336,33 @@ class TestC(unittest.TestCase):
         
         self.assertEqual(data['parse']['domain_status'][0], 'Reserved Domain')
     
-    def test_reserved_domain_cv(self):
-        response = client.post(
-            '/api/v1/whois',
-            headers={'X-Requested-With': 'XMLHttpRequest'},
-            json={"domain": "whois.cv"},
-        )
-        data = json.loads(response.content)
+    # def test_reserved_domain_cv(self):
+    #     response = client.post(
+    #         '/api/v1/whois',
+    #         headers={'X-Requested-With': 'XMLHttpRequest'},
+    #         json={"domain": "whois.cv"},
+    #     )
+    #     data = json.loads(response.content)
         
-        if not data['status']:
-            print('Please check .cv whois server - Reserved Domains!')
-            return
+    #     if not data['status']:
+    #         print('Please check .cv whois server - Reserved Domains!')
+    #         return
         
-        resp_data = data.get('result', '')
-        if resp_data.find('Server can\'t process your request at the moment') > -1:
-            print('The .cv whois server was busy!')
-            return
+    #     resp_data = data.get('result', '')
+    #     if resp_data.find('Server can\'t process your request at the moment') > -1:
+    #         print('The .cv whois server was busy!')
+    #         return
         
-        self.assertEqual(data['parse']['registrar'], '')
-        self.assertEqual(data['parse']['registrar_url'], '')
-        self.assertEqual(len(data['parse']['domain_status']), 1)
-        self.assertEqual(len(data['parse']['nameservers']), 0)
+    #     self.assertEqual(data['parse']['registrar'], '')
+    #     self.assertEqual(data['parse']['registrar_url'], '')
+    #     self.assertEqual(len(data['parse']['domain_status']), 1)
+    #     self.assertEqual(len(data['parse']['nameservers']), 0)
         
-        self.assertEqual(data['parse']['creation_date'], '')
-        self.assertEqual(data['parse']['updated_date'], '')
-        self.assertEqual(data['parse']['expiry_date'], '')
+    #     self.assertEqual(data['parse']['creation_date'], '')
+    #     self.assertEqual(data['parse']['updated_date'], '')
+    #     self.assertEqual(data['parse']['expiry_date'], '')
         
-        self.assertEqual(data['parse']['domain_status'][0], 'Reserved Domain')
+    #     self.assertEqual(data['parse']['domain_status'][0], 'Reserved Domain')
     
     def test_reserved_domain_cx(self):
         response = client.post(
