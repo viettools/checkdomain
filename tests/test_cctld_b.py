ab2e8bbf07ab1dc4df0d3848e14ff4df34233840
Mai Tien Dung <git@minhvuc.com>
2025-08-22 09:20:30 +0700

    [FIX] Fix unittest BW

--- tests/test_cctld_b.py
+++ tests/test_cctld_b.py
@@ -190,7 +190,7 @@ class TestB(unittest.TestCase):
         self.assertGreater(len(data['parse']['domain_status']), 0)
         self.assertGreater(len(data['parse']['nameservers']), 0)
         
-        self.assertEqual(data['parse']['creation_date'], '2012-11-12T22:00:00.0Z')
+        self.assertEqual(data['parse']['creation_date'], '2012-11-12T22:00:00Z')
         self.assertGreater(len(data['parse']['updated_date']), 0)
         self.assertGreater(len(data['parse']['expiry_date']), 0)
         
